e6757f6e3785e91f9e3b3025564752e8d4631ddf
Eliyahu hillel <hillel.eli396@gmail.com>
2019-03-21 11:58:17 +0200

    change nav

--- frontend/src/components/CardPreview.vue
+++ frontend/src/components/CardPreview.vue
@@ -28,14 +28,6 @@ export default {
 </script>
 <style>
 .title-card {
-<<<<<<< HEAD
-    padding: 30px;
-    width: 150px;
-    background-color: rgb(255, 255, 255);
-    border: 1px solid black;
-    font-size: 22px;
-    margin: 5px;
-=======
   padding: 30px;
   width: 150px;
   background-color: rgb(255, 255, 255);
@@ -45,6 +37,5 @@ export default {
 }
 .card-preview {
   cursor: pointer;
->>>>>>> 25a9c34e3ec5f34905590e512e4fc44c2d5e5a71
 }
 </style>

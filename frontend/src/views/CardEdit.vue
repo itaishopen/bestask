4c82be7abf617d3d66c674fb87faa4ed4fda0368
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-21 08:57:57 +0200

    Update CardEdit.vue

--- frontend/src/views/CardEdit.vue
+++ frontend/src/views/CardEdit.vue
@@ -1,52 +1,70 @@
 <template>
     <section>
-        <button v-on:click="editCard">Edit Card</button>
-        <!-- <modal v-if="showModal"></modal> -->
-        <modal v-if="showModal" v-on:close="closeModal" v-on:save="saveCard">
-            <h1 slot="header">Edit Card</h1>
-            <div class="container flex" slot="body">
-                <div class="content flex">
-                    <input type="text" v-model="card.title" placeholder="Title">
-                    <!-- <label>Description</label> -->
-                    <textarea
-                        rows="5"
-                        cols="30"
-                        v-model="card.description"
-                        placeholder="Description"
-                    ></textarea>
-                    <!-- <label>Add Comment</label> -->
-                    <input type="text" v-model="comment" placeholder="Add comment">
-                    <button v-on:click="addComment">Save</button>
-                    <div v-for="comment in card.comments" :key="comment" class="flex">
-                        <div>{{comment}}</div>
+        <!-- <b-button v-on:click="editCard">Edit Card</b-button> -->
+        <div>
+            <b-button v-b-modal.modal1>Edit Card</b-button>
+
+            <!-- Modal Component -->
+            <b-modal id="modal1" title="Edit Card">
+                <div class="container flex">
+                    <main class="content flex">
+                        <b-form-input class="m-1" v-model="card.title" placeholder="Title"/>
+                        <b-form-textarea
+                            class="m-1"
+                            id="textarea"
+                            v-model="card.description"
+                            placeholder="Description"
+                            rows="6"
+                            max-rows="10"
+                        />
+                        <!-- <div class="comments"> -->
+                        <b-form-input class="m-1" v-model="comment" placeholder="Add comment"/>
+                        <b-button class="m-1" v-on:click="addComment">Save</b-button>
+                        <div v-for="comment in card.comments" :key="comment">
+                            {{comment}}
+                            <!-- <div class="flex comments"></div> -->
+                        </div>
+                        <!-- </div> -->
+                    </main>
+                    <div class="nav flex">
+                        <label class="m-1">Add to Card</label>
+                        <b-button class="m-1 btn-block">Members</b-button>
+                        <b-button class="m-1 btn-block">Labels</b-button>
+                        <b-button class="m-1 btn-block">Checklist</b-button>
+                        <label class="m-1">Actions</label>
+                        <b-button class="m-1 btn-block">Move</b-button>
+                        <b-button class="m-1 btn-block">Copy</b-button>
+                        <b-button class="m-1 btn-block">Archive</b-button>
+                        <b-button class="m-1 btn-block">Share</b-button>
                     </div>
                 </div>
-                <div class="nav flex">
-                    <label>Add to Card</label>
-                    <button>Members</button>
-                    <button>Labels</button>
-                    <button>Checklist</button>
-                    <label>Actions</label>
-                    <button>Move</button>
-                    <button>Copy</button>
-                    <button>Archive</button>
-                    <button>Share</button>
+
+                <!-- <p class="my-4">Hello from modal!</p> -->
+                <div slot="modal-footer" class="w-100">
+                    <!-- <p class="float-left">Modal Footer Content</p> -->
+                    <b-button class="float-right" variant="primary" @click="saveCard">Save</b-button>
                 </div>
-            </div>
-        </modal>
+            </b-modal>
+        </div>
     </section>
 </template>
 
 <script>
-import Modal from '../components/Modal.vue';
+// import Modal from '../components/Modal.vue';
 import moment from 'moment';
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 export default {
     name: 'CardEdit',
     data() {
         return {
             showModal: false,
-            comment: ''
+            comment: '',
+            disabledSaveBtn: true
         };
     },
     created() {
@@ -62,7 +80,7 @@ export default {
             set(cardItem) { this.$store.commit('setCardItem', { cardItem }) }
         },
         formattedDate() {
-            return
+            // return
         }
     },
     methods: {
@@ -77,59 +95,66 @@ export default {
             this.showModal = false;
         },
         addComment() {
-            var date = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a');
-            var comment = this.comment + '\t' + date;
-            this.card.comments.unshift(comment);
-            this.comment = '';
+            if (this.comment) {
+                var date = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a');
+                var comment = this.comment + '\t' + date;
+                this.card.comments.unshift(comment);
+                this.comment = '';
+            }
         },
+        toggleSave() {
+            if (this.comment) disabledSaveBtn = true;
+            disabledSaveBtn = false;
+        }
 
     },
     components: {
-        Modal
+        // Modal
     },
 };
 </script>
 
-<style lang="scss" scoped>
-.dialog {
-    width: 100%;
-    max-height: min-content;
-    // height: 100vh;
-}
+<style lang="css" scoped>
 .flex {
     display: flex;
 }
 
 .main-container {
-    // display: flex;
-    flex-direction: column;
-    height: 100vh;
-    // background-color: yellow;
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* height: 100vh; */
+    /* background-color: yellow; */
 }
 
 .header {
-    height: 5em;
-    // background-color: #1E2129;
+    /* height: 5em; */
+    /* background-color: #1E2129; */
 }
 
 .container {
-    // display: flex;
-    // flex-direction: column;
-    // flex: 1;
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* flex: 1; */
     /* width: 100%; */
-    // background-color: blue;
-    justify-content: space-evenly;
+    /* background-color: blue; */
+    justify-content: space-between;
 }
 
 .content {
     flex-direction: column;
     flex-basis: 80%;
     align-items: flex-start;
-    // background-color: #46A3A3;
+    margin: 5px;
+    /* background-color: #46A3A3; */
 }
 
 .nav {
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
+    margin: 5px;
+}
+
+.comments {
+    flex-direction: column;
 }
 </style>

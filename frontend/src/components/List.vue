b452a7338dbcba5a964e0a33cf80f78f6e1a21f7
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-22 18:17:06 +0200

    DragAndDrop

--- frontend/src/components/List.vue
+++ frontend/src/components/List.vue
@@ -1,11 +1,24 @@
 <template>
     <section class="list">
         <div class="title-list">{{list.title}}</div>
-        <ul class="list-cards">
+        <draggable
+            v-model="list.cards"
+            group="list"
+            @start="drag=true"
+            @end="endMoveCard"
+            :move="moveCard"
+        >
+            <transition-group>
+                <div v-for="card in list.cards" :key="card._id">
+                    <card-preview v-if="!card.archived" :card="card"></card-preview>
+                </div>
+            </transition-group>
+        </draggable>
+        <!-- <ul class="list-cards">
             <li v-for="card in list.cards" :key="card._id">
                 <card-preview v-if="!card.archived" :card="card"></card-preview>
             </li>
-        </ul>
+        </ul>-->
         <button v-if="!isAddClick" @click="newCard">
             add card
             <i class="fa fa-plus"></i>
@@ -25,20 +38,51 @@
 <script>
 import CardPreview from "@/components/CardPreview.vue";
 import CardService from "../services/CardService.js";
+import draggable from 'vuedraggable';
+
 export default {
     name: "list",
     props: ["list"],
     data() {
         return {
-            isAddClick: false
+            isAddClick: false,
+            moveCardId: null,
+            fromListId: null,
+            toListId: null
             // cardTitle: null,
             // currList: null
         };
     },
     components: {
-        CardPreview
+        CardPreview,
+        draggable
     },
     methods: {
+        moveCard(evt) {
+            console.log('moveCard');
+            // console.log(evt);
+            this.moveCardId = evt.draggedContext.element._id;
+            this.fromListId = evt.draggedContext.element.listId;
+            this.toListId = evt.relatedContext.element.listId;
+        },
+        endMoveCard(evt) {
+            console.log('onEnd');
+            console.log(this.card);
+            if (this.fromListId !== this.toListId) {
+                this.$store.dispatch({ type: 'loadCard', cardId: this.moveCardId });
+                this.card.listId = this.toListId;
+                this.$store.dispatch({ type: 'saveCard', card: this.card })
+                    .then(res => {
+                        console.log(res);
+                        // EventBusService.$emit(SHOW_MSG, { txt: 'Card Saved!', type: 'success' });
+                        // this.$router.push('/task');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        // this.$router.push('/task');
+                    });
+            }
+        },
         newCard() {
             // this.cardTitle = CardService.getEmpty();
             console.log("new card", this.card);

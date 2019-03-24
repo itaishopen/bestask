a8782a941a0968925df1dfaa5938d319c8af387a
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-24 17:57:17 +0200

    draggable Lists

--- frontend/src/components/Board.vue
+++ frontend/src/components/Board.vue
@@ -1,47 +1,57 @@
 <template>
-  <section class="board">
-    <div class="board-title" v-if="!isChangeTitle" @click.prevent="choseTitle">{{board.title}}</div>
-    <form v-if="isChangeTitle" @submit.prevent="changeTitle" class="form-add">
-      <input
-        class="input-title-board"
-        ref="title"
-        v-model="board.title"
-        placeholder="Enter title here..."
-      >
-      <button class="btn-title-board" type="submit">
-        <i class="fa fa-plus"></i>
-      </button>
-    </form>
-    <ul class="board-list-ul">
-      <!-- <draggable v-model="lists" group="lists" @start="drag=true" @end="drag=false"> -->
-      <li class="board-list-li" v-for="list in lists" :key="list._id">
-        <list :list="list"/>
-      </li>
-      <!-- </draggable> -->
-      <div class="create-list">
-        <button v-if="!isAddListClick" class="create-list-title" @click="newList">
-          Add new list
-          <i class="fa fa-plus"></i>
-        </button>
-
-        <form
-          v-if="isAddListClick"
-          @submit.prevent="addList"
-          class="create-list-input form-add-list"
-        >
-          <div>
-            <input class="input-new-list" v-model="list.title" placeholder="Enter text here...">
-          </div>
-          <div class="container-add-list-btns">
-            <button class="create-list-options list-new-list-options" type="submit">
-              <i class="fa fa-plus"></i>
+    <section class="board">
+        <div class="board-title" v-if="!isChangeTitle" @click.prevent="choseTitle">{{board.title}}</div>
+        <form v-if="isChangeTitle" @submit.prevent="changeTitle" class="form-add">
+            <input
+                class="input-title-board"
+                ref="title"
+                v-model="board.title"
+                placeholder="Enter title here..."
+            >
+            <button class="btn-title-board" type="submit">
+                <i class="fa fa-plus"></i>
             </button>
-            <button class="list-x-list-options" @click="closeAdd">&times;</button>
-          </div>
         </form>
-      </div>
-    </ul>
-  </section>
+        <ul class="board-list-ul">
+            <draggable
+                v-model="lists"
+                group="lists"
+                @start="drag=true"
+                @end="endMoveList"
+                :move="moveList"
+            >
+                <li class="board-list-li" v-for="list in lists" :key="list._id">
+                    <list :list="list"/>
+                </li>
+            </draggable>
+            <div class="create-list">
+                <button v-if="!isAddListClick" class="create-list-title" @click="newList">
+                    Add new list
+                    <i class="fa fa-plus"></i>
+                </button>
+
+                <form
+                    v-if="isAddListClick"
+                    @submit.prevent="addList"
+                    class="create-list-input form-add-list"
+                >
+                    <div>
+                        <input
+                            class="input-new-list"
+                            v-model="list.title"
+                            placeholder="Enter text here..."
+                        >
+                    </div>
+                    <div class="container-add-list-btns">
+                        <button class="create-list-options list-new-list-options" type="submit">
+                            <i class="fa fa-plus"></i>
+                        </button>
+                        <button class="list-x-list-options" @click="closeAdd">&times;</button>
+                    </div>
+                </form>
+            </div>
+        </ul>
+    </section>
 </template>
 
 <script>
@@ -52,206 +62,221 @@ import list from "./List.vue";
 import draggable from "vuedraggable";
 
 export default {
-  name: "board",
-  props: ["board"],
-  data() {
-    return {
-      isAddListClick: false,
-      isChangeTitle: false,
-    };
-  },
-  components: {
-    list,
-    draggable
-  },
+    name: "board",
+    props: ["board"],
+    data() {
+        return {
+            isAddListClick: false,
+            isChangeTitle: false,
+        };
+    },
+    components: {
+        list,
+        draggable
+    },
 
-  computed: {
-    lists: {
-      get() {
-        return this.$store.getters.getLists;
-      },
-      set(lists) {
-        this.$store.commit("setLists", { lists: lists });
-      }
+    computed: {
+        lists: {
+            get() {
+                return this.$store.getters.getLists;
+            },
+            set(lists) {
+                this.$store.commit("setLists", { lists: lists });
+            }
+        }
+    },
+
+    methods: {
+        newList() {
+            //  todo: add list
+            this.list = ListService.getEmptyList();
+            this.isAddListClick = !this.isAddListClick;
+        },
+        closeAdd() {
+            this.isAddListClick = !this.isAddListClick;
+        },
+        addList() {
+            this.list.boardId = this.board._id;
+            this.list.order = this.lists.length;
+            this.$store
+                .dispatch({ type: "saveNewList", list: this.list })
+                .then(savedList => {
+                    let activity = ActivityService.getEmptyActivity();
+                    activity.text = " added a new list to ";
+                    activity.userId = this.$store.getters.loggedInUser._id;
+                    activity.boardId = this.board._id;
+                    activity.listId = savedList._id;
+                    this.$store.dispatch({ type: "saveActivity", activity });
+                });
+            this.isAddListClick = !this.isAddListClick;
+        },
+        closeAdd() {
+            this.isAddListClick = !this.isAddListClick;
+        },
+        addList() {
+            this.list.boardId = this.board._id;
+            this.list.order = this.lists.length + 1;
+            console.log(this.list, "list in add click");
+            this.$store
+                .dispatch({ type: "saveNewList", list: this.list })
+                .then(savedList => {
+                    let activity = ActivityService.getEmptyActivity();
+                    activity.text = " added a new list to ";
+                    activity.userId = this.$store.getters.loggedInUser._id;
+                    activity.boardId = this.board._id;
+                    activity.listId = savedList._id;
+                    this.$store.dispatch({ type: "saveActivity", activity });
+                });
+            this.isAddListClick = !this.isAddListClick;
+        },
+        choseTitle() {
+            console.log("isChangeTitle", this.isChangeTitle);
+            this.isChangeTitle = !this.isChangeTitle;
+        },
+        changeTitle() {
+            console.log("this.board", this.board);
+            this.$store.dispatch({ type: "saveBoard", board: this.board });
+            this.isChangeTitle = !this.isChangeTitle;
+        },
+        moveList(evt) {
+            // console.log(evt);
+
+        },
+        endMoveList(evt) {
+            for (var i = 0; i < this.lists.length; i++) {
+                this.lists[i].order = i;
+            }
+            this.$store.dispatch({ type: "updateLists", lists: this.lists });
+        }
+    },
+
+    watch: {
+        // board: function () {
+        //     console.log("change in board", this.board);
+        //     this.$store.dispatch({ type: "saveBoard", board: this.board });
+        // }
     }
-  },
-
-  methods: {
-    newList() {
-      //  todo: add list
-      this.list = ListService.getEmptyList();
-      this.isAddListClick = !this.isAddListClick;
-    },
-    closeAdd() {
-      this.isAddListClick = !this.isAddListClick;
-    },
-    addList() {
-      this.list.boardId = this.board._id;
-      this.list.order = this.lists.length;
-      this.$store
-        .dispatch({ type: "saveNewList", list: this.list })
-        .then(savedList => {
-          let activity = ActivityService.getEmptyActivity();
-          activity.text = " added a new list to ";
-          activity.userId = this.$store.getters.loggedInUser._id;
-          activity.boardId = this.board._id;
-          activity.listId = savedList._id;
-          this.$store.dispatch({ type: "saveActivity", activity });
-        });
-      this.isAddListClick = !this.isAddListClick;
-    },
-    closeAdd() {
-      this.isAddListClick = !this.isAddListClick;
-    },
-    addList() {
-      this.list.boardId = this.board._id;
-      this.list.order = this.lists.length + 1;
-      console.log(this.list, "list in add click");
-      this.$store
-        .dispatch({ type: "saveNewList", list: this.list })
-        .then(savedList => {
-          let activity = ActivityService.getEmptyActivity();
-          activity.text = " added a new list to ";
-          activity.userId = this.$store.getters.loggedInUser._id;
-          activity.boardId = this.board._id;
-          activity.listId = savedList._id;
-          this.$store.dispatch({ type: "saveActivity", activity });
-        });
-      this.isAddListClick = !this.isAddListClick;
-    },
-    choseTitle() {
-      console.log("isChangeTitle", this.isChangeTitle);
-      this.isChangeTitle = !this.isChangeTitle;
-    },
-    changeTitle() {
-      console.log("this.board", this.board);
-      this.$store.dispatch({ type: "saveBoard", board: this.board });
-      this.isChangeTitle = !this.isChangeTitle;
-    }
-  },
-
-  watch: {
-    // board: function () {
-    //     console.log("change in board", this.board);
-    //     this.$store.dispatch({ type: "saveBoard", board: this.board });
-    // }
-  }
 };
 </script>
 
 <style lang="scss">
 .board-title {
-  display: flex;
-  padding: 15px;
-  font-family: Lato_bold;
-  font-size: 20px;
+    display: flex;
+    padding: 15px;
+    font-family: Lato_bold;
+    font-size: 20px;
 }
 .board-list-li {
-  min-height: 80vh;
-  max-height: 80vh;
-  min-width: 280px;
-  overflow: auto;
-  /* max-height: 100vh; */
-  background-color: rgb(235, 235, 235);
-  border: 1px solid rgb(206, 206, 206);
-  border-radius: 8px;
-  margin: 0px 5px;
+    min-height: 80vh;
+    max-height: 80vh;
+    min-width: 280px;
+    overflow: auto;
+    /* max-height: 100vh; */
+    background-color: rgb(235, 235, 235);
+    border: 1px solid rgb(206, 206, 206);
+    border-radius: 8px;
+    margin: 0px 5px;
 }
 .board-list-ul {
-  /* margin: 0 auto; */
-  width: min-content;
+    /* margin: 0 auto; */
+    width: min-content;
 
-  background-color: rgb(255, 255, 255);
-  /* border: 1px solid black; */
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-start;
+    background-color: rgb(255, 255, 255);
+    /* border: 1px solid black; */
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
 }
 .create-list-title {
-  min-width: 280px;
-  height: 100px;
-  background-color: rgba(255, 255, 255, 0.075);
-  border: none;
-  font-size: 20px;
-  color: rgb(82, 82, 82);
-  display: flex;
-  justify-content: center;
+    min-width: 280px;
+    height: 100px;
+    background-color: rgba(255, 255, 255, 0.075);
+    border: none;
+    font-size: 20px;
+    color: rgb(82, 82, 82);
+    display: flex;
+    justify-content: center;
 }
 .create-list-title:hover {
-  color: rgb(22, 22, 22);
+    color: rgb(22, 22, 22);
 }
 
 .form-add-list {
-  background-color: rgba(255, 255, 255, 0);
-  display: flex;
+    background-color: rgba(255, 255, 255, 0);
+    display: flex;
 }
 .list-add-list {
-  padding: 10px 0;
-  width: 270px;
-  height: 40px;
-  background-color: rgba(255, 255, 255, 0);
-  border: none;
-  font-size: 20px;
-  color: rgb(82, 82, 82);
-  border-radius: 7px;
-  transition: 0.3s;
+    padding: 10px 0;
+    width: 270px;
+    height: 40px;
+    background-color: rgba(255, 255, 255, 0);
+    border: none;
+    font-size: 20px;
+    color: rgb(82, 82, 82);
+    border-radius: 7px;
+    transition: 0.3s;
 }
 .form-add-list {
-  width: 270px;
-  height: 100px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  background-color: #ebebeb;
-  border: 1px solid #cecece;
-  border-radius: 8px;
+    width: 270px;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    background-color: #ebebeb;
+    border: 1px solid #cecece;
+    border-radius: 8px;
 
-  margin: 3px;
+    margin: 3px;
 }
 .input-new-list {
-  min-width: 264px;
-  height: 40px;
-  border: none;
-  border: 1px solid rgb(230, 230, 230);
-  border-radius: 10px;
-  padding: 5px;
-  margin-top: 10px;
+    min-width: 264px;
+    height: 40px;
+    border: none;
+    border: 1px solid rgb(230, 230, 230);
+    border-radius: 10px;
+    padding: 5px;
+    margin-top: 10px;
 }
 .container-list-card-btns {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
 }
 .list-add-list:hover {
-  background-color: rgba(199, 199, 199, 0);
-  color: rgb(0, 0, 0);
+    background-color: rgba(199, 199, 199, 0);
+    color: rgb(0, 0, 0);
 }
 .list-new-list-options {
-  background-color: rgb(51, 236, 66);
-  color: rgb(255, 255, 255);
-  border: none;
-  border-radius: 5px;
-  padding: 8px;
-  margin: 0 3px;
+    background-color: rgb(51, 236, 66);
+    color: rgb(255, 255, 255);
+    border: none;
+    border-radius: 5px;
+    padding: 8px;
+    margin: 0 3px;
 }
 .list-x-list-options {
-  background-color: rgba(51, 236, 66, 0);
-  border: none;
-  width: 20px;
+    background-color: rgba(51, 236, 66, 0);
+    border: none;
+    width: 20px;
 }
 .input-title-board {
-  font-size: 18px;
-  font-weight: bold;
-  height: 32px;
-  width: 300px;
-  border: none;
-  background: rgba(255, 255, 255, 0.911);
-  padding-left: 10px;
+    font-size: 18px;
+    font-weight: bold;
+    height: 32px;
+    width: 300px;
+    border: none;
+    background: rgba(255, 255, 255, 0.911);
+    padding-left: 10px;
 }
 .btn-title-board {
-  background: rgba(255, 255, 255, 0.911);
-  border: none;
+    background: rgba(255, 255, 255, 0.911);
+    border: none;
+}
+
+.draggable {
+    display: flex;
+    flex-direction: row;
 }
 </style>
 

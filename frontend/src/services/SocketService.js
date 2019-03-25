dd51a6453f7975900f013b55bae3fb9158be86e0
itaishopen <itaishopen@gmail.com>
2019-03-25 18:43:41 +0200

    socket

--- frontend/src/services/SocketService.js
+++ frontend/src/services/SocketService.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
-const socket = io('http://localhost:3000');
+// const port = process.env.PORT || 3000;
+const socket = io(':3000');
 // var user = this.$store.getters.loggedInUser
 
 function init(boardId){

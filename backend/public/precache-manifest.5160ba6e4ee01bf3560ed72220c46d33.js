36f75b6485c0841fb6ecc5284b8868b9eb78640e
itaishopen <itaishopen@gmail.com>
2019-03-27 10:15:18 +0200

    backend dev

---
+++ backend/public/precache-manifest.5160ba6e4ee01bf3560ed72220c46d33.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e2160a3481cc78b7684014fadcc1c3",
+    "url": "/fonts/FingerPaint-Regular.66e2160a.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "54f7b1a951e2c6c3884bdc803ae03c25",
+    "url": "/fonts/PontanoSans-Regular.54f7b1a9.ttf"
+  },
+  {
+    "revision": "c2a181094ec6b8961dfd",
+    "url": "/js/app.50a22405.js"
+  },
+  {
+    "revision": "9581bb2813c1c99b1117",
+    "url": "/js/chunk-vendors.ea5a73fb.js"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/fonts/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "6126996ef09a5371cfa5",
+    "url": "/js/about.f0b58730.js"
+  },
+  {
+    "revision": "11b9e3ccc299e011df0904331bb6cabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c201fcc35f571b4bb800441dc1543e9",
+    "url": "/img/bestask-logo-black.6c201fcc.png"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/fonts/Lato_Regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "eb9532033c2adf99b1314611b5e9cd0e",
+    "url": "/fonts/Lato_Bold.eb953203.ttf"
+  },
+  {
+    "revision": "d0383190098a91bba6903ed5b9f70a8f",
+    "url": "/fonts/EmilysCandy-Regular.d0383190.ttf"
+  },
+  {
+    "revision": "0511c175bbf74b3c5ce97c6790343e69",
+    "url": "/fonts/BerkshireSwash-Regular.0511c175.ttf"
+  },
+  {
+    "revision": "cf2842ec05dc062e6a7c67ff09e83fd8",
+    "url": "/fonts/Acme-Regular.cf2842ec.ttf"
+  },
+  {
+    "revision": "9581bb2813c1c99b1117",
+    "url": "/css/chunk-vendors.172ce37f.css"
+  },
+  {
+    "revision": "c2a181094ec6b8961dfd",
+    "url": "/css/app.caa8f50b.css"
+  }
+];

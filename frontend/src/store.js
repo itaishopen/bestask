f81d3d06f7a9d5b2e8ee2152ad49d6bc1510e015
itaishopen <itaishopen@gmail.com>
2019-03-27 10:04:16 +0200

    card store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import cardStore from './modules/cardStore.js';
 import userStore from './modules/userStore.js';
 import boardStore from './modules/boardStore.js';
 
@@ -8,7 +7,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        cardStore,
         userStore,
         boardStore
     },
